4e87f73eafeb08618105c1157bd884c668917939
akl7777777 <akl7777777@163.com>
2023-03-06 22:44:58 +0800

    整体重构,突破200上限,支持多家服务商切换,翻译更流畅,不报错

---
+++ src/b.js
@@ -0,0 +1,166 @@
+function n(t, e) {
+    for (var n = 0; n < e.length - 2; n += 3) {
+        var r = e.charAt(n + 2);
+        r = "a" <= r ? r.charCodeAt(0) - 87 : Number(r),
+            r = "+" === e.charAt(n + 1) ? t >>> r : t << r,
+            t = "+" === e.charAt(n) ? t + r & 4294967295 : t ^ r
+    }
+    return t
+}
+
+function encrypt(t, gtk) {
+    //  "320305.131321201";
+    console.log(t, gtk)
+    let r = gtk
+    var o, i = t.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g);
+    if (null === i) {
+        var a = t.length;
+        a > 30 && (t = "".concat(t.substr(0, 10)).concat(t.substr(Math.floor(a / 2) - 5, 10)).concat(t.substr(-10, 10)))
+    } else {
+        for (var s = t.split(/[\uD800-\uDBFF][\uDC00-\uDFFF]/), c = 0, l = s.length, u = []; c < l; c++)
+            "" !== s[c] && u.push.apply(u, function (t) {
+                if (Array.isArray(t))
+                    return e(t)
+            }(o = s[c].split("")) || function (t) {
+                if ("undefined" != typeof Symbol && null != t[Symbol.iterator] || null != t["@@iterator"])
+                    return Array.from(t)
+            }(o) || function (t, n) {
+                if (t) {
+                    if ("string" == typeof t)
+                        return e(t, n);
+                    var r = Object.prototype.toString.call(t).slice(8, -1);
+                    return "Object" === r && t.constructor && (r = t.constructor.name),
+                        "Map" === r || "Set" === r ? Array.from(t) : "Arguments" === r || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r) ? e(t, n) : void 0
+                }
+            }(o) || function () {
+                throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")
+            }()),
+            c !== l - 1 && u.push(i[c]);
+        var p = u.length;
+        p > 30 && (t = u.slice(0, 10).join("") + u.slice(Math.floor(p / 2) - 5, Math.floor(p / 2) + 5).join("") + u.slice(-10).join(""))
+    }
+    for (var d = "".concat(String.fromCharCode(103)).concat(String.fromCharCode(116)).concat(String.fromCharCode(107)), h = (null !== r ? r : (r = window[d] || "") || "").split("."), f = Number(h[0]) || 0, m = Number(h[1]) || 0, g = [], y = 0, v = 0; v < t.length; v++) {
+        var _ = t.charCodeAt(v);
+        _ < 128 ? g[y++] = _ : (_ < 2048 ? g[y++] = _ >> 6 | 192 : (55296 == (64512 & _) && v + 1 < t.length && 56320 == (64512 & t.charCodeAt(v + 1)) ? (_ = 65536 + ((1023 & _) << 10) + (1023 & t.charCodeAt(++v)),
+            g[y++] = _ >> 18 | 240,
+            g[y++] = _ >> 12 & 63 | 128) : g[y++] = _ >> 12 | 224,
+            g[y++] = _ >> 6 & 63 | 128),
+            g[y++] = 63 & _ | 128)
+    }
+    for (var b = f, w = "".concat(String.fromCharCode(43)).concat(String.fromCharCode(45)).concat(String.fromCharCode(97)) + "".concat(String.fromCharCode(94)).concat(String.fromCharCode(43)).concat(String.fromCharCode(54)), k = "".concat(String.fromCharCode(43)).concat(String.fromCharCode(45)).concat(String.fromCharCode(51)) + "".concat(String.fromCharCode(94)).concat(String.fromCharCode(43)).concat(String.fromCharCode(98)) + "".concat(String.fromCharCode(43)).concat(String.fromCharCode(45)).concat(String.fromCharCode(102)), x = 0; x < g.length; x++)
+        b = n(b += g[x], w);
+    return b = n(b, k),
+    (b ^= m) < 0 && (b = 2147483648 + (2147483647 & b)),
+        "".concat((b %= 1e6).toString(), ".").concat(b ^ f)
+}
+
+
+async function translate(query, source_lang, target_lang, translate_text, completion) {
+    // 特殊处理把cn替换为zh
+    if (source_lang == 'cn') {
+        source_lang = 'zh'
+    }
+    if (target_lang == 'cn') {
+        target_lang = 'zh'
+    }
+    try {
+        const url = 'https://fanyi.baidu.com/';
+        // 第一轮取cookie
+        const respCookie = await $http.request({
+            method: "GET",
+            url: url,
+            header: {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cache-Control': 'no-cache',
+                'Connection': 'keep-alive',
+                'Host': 'fanyi.baidu.com',
+                'Pragma': 'no-cache',
+                'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+            }
+        });
+        $log.error('#################respCookie==>' + JSON.stringify(respCookie))
+        $log.error('#################respCookie.response==>' + JSON.stringify(respCookie.response))
+        $log.error('#################respCookie.response.headers==>' + JSON.stringify(respCookie.response.headers))
+        // const cookie = respCookie.headers['Set-Cookie']
+        // $log.error('#################cookie==>' + JSON.stringify(cookie))
+
+        // 第二轮取gtk,token
+        const respTks = await $http.request({
+            method: "GET",
+            header: {
+                // 'Cookie': cookie,
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+
+            },
+            url: url
+        });
+        const gtk = respTks.data.match(/window\.gtk\s*=\s*"([\d.]+)"/)[1];
+        const token = respTks.data.match(/token:\s*'(\w+)'/)[1];
+        $log.error('################# gtk ==>' + gtk)
+        $log.error('################# token ==>' + token)
+        const headers = {
+            // Cookie: cookie,
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+        };
+        const sign = encrypt(translate_text, gtk)
+        $log.error('################# sign ==>' + token)
+
+        const data = {
+            from: source_lang,
+            to: target_lang,
+            query: translate_text,
+            transtype: 'realtime',
+            simple_means_flag: '3',
+            sign: sign,
+            token: token
+        };
+        const resp = await $http.request({
+            method: "POST",
+            url: 'https://fanyi.baidu.com/v2transapi?from=' + source_lang + '&to=' + target_lang,
+            header: headers,
+            body: data
+        });
+        if (resp.data && resp.data.trans_result && resp.data.trans_result.data.length && resp.data.trans_result.data[0].dst) {
+            const rs = []
+            resp.data.trans_result.data.forEach(function (item) {
+                rs.push(item.dst)
+            })
+            completion({
+                result: {
+                    from: query.detectFrom,
+                    to: query.detectTo,
+                    toParagraphs: rs,
+                },
+            });
+        } else {
+            const errMsg = resp.data ? JSON.stringify(resp.data) : '请求翻译接口失败,请检查网络'
+            completion({
+                error: {
+                    type: 'unknown',
+                    message: errMsg,
+                    addtion: errMsg,
+                },
+            });
+        }
+    } catch (e) {
+        $log.error('接口请求错误 ==> ' + JSON.stringify(e))
+        Object.assign(e, {
+            _type: 'network',
+            _message: '接口请求错误 - ' + JSON.stringify(e),
+        });
+        throw e;
+    }
+}
+
+exports.translate = translate;
---
+++ src/d.js
@@ -0,0 +1,110 @@
+var CryptoJS = require("crypto-js");
+
+// 加密
+function btoa(str) {
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(str));
+}
+
+// 解密
+function atob(str) {
+    return CryptoJS.enc.Base64.parse(str).toString(CryptoJS.enc.Utf8);
+}
+
+
+async function translate(query, source_lang, target_lang, translate_text, completion) {
+    // 获取登录令牌
+    const loginUrl = 'http://track.hujiang.com/v4/track'
+    // const url = 'http://res.d.hjfile.cn/v10/dict/translation/cn/en?content=hi';
+    const url = 'http://res.d.hjfile.cn/v10/dict/translation/' + source_lang + '/' + target_lang;
+    const loginHeaders = {
+        'Accept': 'application/json, text/javascript, */*;',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cache-Control': 'no-cache',
+        // 'Content-Length': '1910',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        // 'Host': 'track.hujiang.com',
+        'Origin': 'http://res.d.hjfile.cn',
+        'Pragma': 'no-cache',
+        'Proxy-Connection': 'keep-alive',
+        'Referer': 'http://res.d.hjfile.cn/app/trans',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+    }
+    const translateHeaders = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        // 'Accept-Encoding': 'gzip, deflate',
+        // 'Accept-Language': 'zh-CN,zh;q=0.9',
+        // 'Cache-Control': 'no-cache',
+        // 'Content-Length': '1910',
+        'Host': 'res.d.hjfile.cn',
+        'Origin': 'http://res.d.hjfile.cn',
+        // 'Pragma': 'no-cache',
+        // 'Proxy-Connection': 'keep-alive',
+        'Referer': 'http://res.d.hjfile.cn/app/trans',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    }
+
+    // const d = btoa(encodeURIComponent('{"hj_siteid":3,"hj_uid":"e2235725-ea43-9572-4cc8-998d7ed13e9b","hj_sid":"7636bbd9-294e-bf2d-5f29-6b800c77a6e9","hj_ssid":"d5d98b26-2a60-91d0-f37b-27b0ce05c2c0","hj_url":"http://res.d.hjfile.cn/app/trans","hj_urlref":"","hjid":0,"hj_p":"{\\"res\\":\\"1440*900\\",\\"wres\\":\\"291*796\\",\\"bdhm\\":null,\\"title\\":\\"%E5%85%A8%E6%96%87%E7%BF%BB%E8%AF%91_%E5%A4%9A%E8%AF%AD%E7%A7%8D%E5%9C%A8%E7%BA%BF%E7%BF%BB%E8%AF%91_%E6%B2%AA%E6%B1%9F%E5%B0%8FD\\"}","hj_vt":0,"hj_sst":1,"hj_csst":1,"hj_st":1,"hj_cst":0,"hj_ref":"","hj_sref":"","hj_fp":1370931257,"hj_v":"1.4.7","hj_ua":"mozilla/5.0 (macintosh; intel mac os x 10_15_7) applewebkit/537.36 (khtml, like gecko) chrome/109.0.0.0 safari/537.36","hj_t":1676825564724,"ch_source":"","hj_sr":"direct","hj_srp":"","hj_src":"","hj_srpt":"","hj_ssr":"direct","hj_ssrp":"","hj_ssrc":"","hj_ssrpt":"","hj_tm":"pc","_":"56496300e94da42f","is_sync":1}'))
+    // TODO 尝试篡改id后,服务端并未报错,说明后端未校验,暂且先用着;后续研究id生成算法,自行生成id,而不应该写死
+    const d = btoa(encodeURIComponent('{"hj_siteid":3,"hj_uid":"e2235234-ea43-9572-4cc8-998d7ed13e9b","hj_sid":"7666bbd9-294e-bf2d-5f29-6b800c77a6e9","hj_ssid":"d5d99b26-2a60-91d0-f37b-27b0ce05c2c0","hj_url":"http://res.d.hjfile.cn/app/trans","hj_urlref":"","hjid":0,"hj_p":"{\\"res\\":\\"1440*900\\",\\"wres\\":\\"291*796\\",\\"bdhm\\":null,\\"title\\":\\"%E5%85%A8%E6%96%87%E7%BF%BB%E8%AF%91_%E5%A4%9A%E8%AF%AD%E7%A7%8D%E5%9C%A8%E7%BA%BF%E7%BF%BB%E8%AF%91_%E6%B2%AA%E6%B1%9F%E5%B0%8FD\\"}","hj_vt":0,"hj_sst":1,"hj_csst":1,"hj_st":1,"hj_cst":0,"hj_ref":"","hj_sref":"","hj_fp":1370931257,"hj_v":"1.4.7","hj_ua":"mozilla/5.0 (macintosh; intel mac os x 10_15_7) applewebkit/537.36 (khtml, like gecko) chrome/109.0.0.0 safari/537.36","hj_t":1676825564724,"ch_source":"","hj_sr":"direct","hj_srp":"","hj_src":"","hj_srpt":"","hj_ssr":"direct","hj_ssrp":"","hj_ssrc":"","hj_ssrpt":"","hj_tm":"pc","_":"56496300e94da42f","is_sync":1}'))
+
+    const loginFormData = {
+        'd': d,
+        't': Date.now()
+    }
+    try {
+        /*const uid_resp = await $http.request({
+          method: "POST",
+          url: loginUrl,
+          header: loginHeaders,
+          body: loginFormData
+        });
+        if (uid_resp.data && uid_resp.data.HJ_UID) {
+          translateHeaders.Cookie = 'HJ_UID='+uid_resp.data.HJ_UID+'; HJC_USRC=uzhi; HJC_NUID=1'
+        } else {
+          const errMsg = uid_resp.data ? JSON.stringify(uid_resp.data) : '获取UID未返回有效结果=>' + JSON.stringify(uid_resp)
+          completion({
+            error: {
+              type: 'unknown',
+              message: errMsg,
+              addtion: errMsg,
+            },
+          });
+        }*/
+        translateHeaders.Cookie = 'HJ_UID=390f25c7-c9f3-b237-f639-62bd23cd431f; HJC_USRC=uzhi; HJC_NUID=1'
+        const resp = await $http.request({
+            method: "POST",
+            url: url,
+            header: translateHeaders,
+            body: {content: translate_text}
+        });
+        if (resp.data && resp.data.data && resp.data.data.content) {
+            completion({
+                result: {
+                    from: query.detectFrom,
+                    to: query.detectTo,
+                    toParagraphs: resp.data.data.content.split('\n'),
+                },
+            });
+        } else {
+            const errMsg = resp.data ? JSON.stringify(resp.data) : '请求翻译接口失败,请检查网络'
+            completion({
+                error: {
+                    type: 'unknown',
+                    message: errMsg,
+                    addtion: errMsg,
+                },
+            });
+        }
+    } catch (e) {
+        $log.error('接口请求错误 ==> ' + JSON.stringify(e))
+        Object.assign(e, {
+            _type: 'network',
+            _message: '接口请求错误 - ' + JSON.stringify(e),
+        });
+        throw e;
+    }
+
+}
+
+exports.translate = translate;
